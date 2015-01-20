201b48f5b05d1ce0230a54e2feb91c3583171dd4
xoxoxo <radoslaw@fateone.com>
2015-01-20 19:26:22 +0000

    fix assets wrong url and append to head

--- loadThat.js
+++ loadThat.js
@@ -5,22 +5,22 @@
 
 function loadThat(url, type, fn) {
 
-  var getBody = document.getElementsByTagName('body')[0];
+  var getHead = document.getElementsByTagName('head')[0];
   var getAssets;
 
   if (type === 'css') {
     getAssets = document.createElement('link');
-    getAssets.href = assetLink;
+    getAssets.href = url;
     getAssets.rel = 'stylesheet';
     getAssets.type = 'text/css';
   } else {
     getAssets = document.createElement('script');
-    getAssets.src = assetLink;
+    getAssets.src = url;
     getAssets.async = true;
     getAssets.type = 'text/javascript';
   }
 
-  getBody.appendChild(getAssets);
+  getHead.appendChild(getAssets);
 
   if (fn !== undefined) {
     getAssets.onload = function(){
