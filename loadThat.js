64f94b280261ef5f7b88297120af2794a9906e03
Radosław Wróblewski <radoslaw@fateone.com>
2016-04-06 01:01:38 +0200

    Update loadThat.js

--- loadThat.js
+++ loadThat.js
@@ -5,7 +5,7 @@
 
 function loadThat(url, type, fn) {
 
-  var getHead = document.getElementsByTagName('head')[0];
+  var getBody = document.getElementsByTagName('body')[0];
   var getAssets;
 
   if (type === 'css') {
@@ -13,19 +13,38 @@ function loadThat(url, type, fn) {
     getAssets.href = url;
     getAssets.rel = 'stylesheet';
     getAssets.type = 'text/css';
+    getBody.appendChild(getAssets);
+    if (fn) {
+      getAssets.onload = function(){
+        fn();
+      };
+    }
   } else {
-    getAssets = document.createElement('script');
-    getAssets.src = url;
-    getAssets.async = true;
-    getAssets.type = 'text/javascript';
-  }
+    if (navigator.appVersion.indexOf("MSIE 10") === -1) {
+      getAssets = document.createElement('script');
+      getAssets.src = url;
+      getAssets.async = true;
+      getAssets.type = 'text/javascript';
+      getBody.appendChild(getAssets);
 
-  getHead.appendChild(getAssets);
-
-  if (fn !== undefined) {
-    getAssets.onload = function(){
-      fn();
-    };
+      if (fn) {
+        getAssets.onload = function(){
+          fn();
+        };
+      }
+    } else {
+      getAssets = new XMLHttpRequest();
+      getAssets.open("GET", url);
+      getAssets.onreadystatechange=function() {
+        if (getAssets.readyState==4 && getAssets.status==200) {
+          JSON.parse(getAssets.responseText);
+          if (fn) {
+            fn();
+          }
+        }
+      };
+      getAssets.send();
+    }
   }
 
 }
